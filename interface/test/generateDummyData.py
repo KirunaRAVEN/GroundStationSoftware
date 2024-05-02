c3aa19dbfd8f8bb79fc3df5f51ea36e486b8c415
TakashiSenpai <louis.barboutie@gmail.com>
2024-05-02 15:09:07 +0200

    add message field entry

--- interface/test/generateDummyData.py
+++ interface/test/generateDummyData.py
@@ -1,3 +1,10 @@
+'''
+    AUTHOR:  Louis-Hendrik Barboutie
+    CONTACT: louis.barboutie@gmail.com, loubar-3@student.ltu.se
+
+    SUMMARY: Generates a csv file for testing purposes. The simulated data is plain sine waves.
+'''
+
 import time
 import csv
 import random
@@ -7,16 +14,25 @@ import numpy as np
 open('dummyData.csv', 'w')
 
 # list for the row elements
-row = []
-for i in range(21):
-    row.append(i)
-t = 0
+nData = 22
+row = [0] * nData
+
+elapsed_time = 0 
+time_increment = 0.01
+old_msg = ' '
 while True:
     with open('dummyData.csv', 'a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        for i in range(0,21):
-           row[i] = 50 * np.sin(t) + 50 # i * random.randrange(-5,10) 
-        row[4] = 10 * np.sin(10*t) + 25
+        for i in range(0, nData):
+           row[i] = 50 * np.sin(elapsed_time) + 50 
+        new_msg = ''
+        for i in range(20):
+            new_msg += 'a' # f'elapsed time: {elapsed_time:.1f}'
+        #if new_msg != old_msg:
+        row[-1] = new_msg
+        #else:
+        #    row[-1] = ' '
+        old_msg = new_msg
         csv_writer.writerow(row)
-        time.sleep(0.01)
-        t = t + 0.01
+        time.sleep(time_increment)
+        elapsed_time = elapsed_time + time_increment
