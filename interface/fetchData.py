7a4bd238aff00f4a4750b03a35caaddd0c41895d
TakashiSenpai <louis.barboutie@gmail.com>
2024-05-15 11:26:42 +0200

    correct gitignore

--- interface/fetchData.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def fetchData():
-    csvData = []
-    with open('data.csv', 'r') as csvFile:
-        csvReader = csv.reader(csvFile)
-        for line in csvFile:
-            break
-    return csvData
